1db0d84ad53cca7e4c8120bb09a6034c513deb12
Kirsi Huhtakallio <kirsi.huhtakallio@gmail.com>
2023-06-06 15:27:05 +0300

    change file name from assignment2_1.cpp to main.cpp

---
+++ w1/assignment2_1/main.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <iostream>
+
+std::string reverse_string(std::string str)
+{
+    for(int i = 0; i < str.length()/2; i++){
+        std::swap(str[i], str[str.length()-i-1]);
+    }
+    return str;
+}
+
+std::string get_string_from_user()
+{
+    std::cout << "Enter a string: " << std::endl;
+    std::string userinput;
+    std:: cin >> userinput;
+    return userinput;
+
+}
+
+int main()
+{
+    std::cout << "Reverse string: "<< reverse_string(get_string_from_user()) << std::endl;
+    return 0;
+
+}
