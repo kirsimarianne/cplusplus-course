accbb1bc7cd29dea677a4f65032c1ade9fee12ba
Kirsi Huhtakallio <kirsi.huhtakallio@gmail.com>
2023-06-26 07:53:11 +0300

    add create_pascal function

--- w3/assignment3_3_1/Pascal/pascal.cpp
+++ w3/assignment3_3_1/Pascal/pascal.cpp
@@ -6,34 +6,27 @@ std::vector<std::vector<int>> create_pascal(int row)
 {
     std::vector<std::vector<int>> vect(row);
 
-    for (int i = 0; i < row; ++i)
+    for (int i = 1; i < row; ++i)
     {
-        int column = i + i - 1;
+        int column = i;
         vect[i].resize(column);
-        // rivin pituus kasvaa joka rivillä kahdella
         vect[i][0] = 1;
-        vect[i][column-1] = 1;
-        for (int j = 1; j < column-1; ++j)
+        vect[i][column - 1] = 1;
+        for (int j = 1; j < column - 1; ++j)
         {
-            if (j % 2 != 0)
-            {
-                vect[i][j] = vect[i-1][j-2] + vect [i-1][j];
-            }
-            else
-            {
-                vect[i][j] = 7;
-            }
-        }
 
-        for(auto row : vect)
-        {
-            for(auto elem : row)
-            {
-                std::cout << elem;
-            }
-            std::cout << "\n";
+            vect[i][j] = vect[i - 1][j-1] + vect[i - 1][j];
         }
     }
+
+    for (auto row : vect)
+        {
+            for (auto elem : row)
+            {
+                std::cout << elem<< " ";
+            }
+            std::cout << "\n\n";
+        }
 
     return vect;
 }
--- w3/assignment3_3_1/Pascal/pascal_tests.cpp
+++ w3/assignment3_3_1/Pascal/pascal_tests.cpp
@@ -5,22 +5,13 @@
 
 TEST_CASE("Pascal vector tests")
 {
-    std::vector<std::vector<int>> result {create_pascal(5)};
+    int value = 5;
+    std::vector<std::vector<int>> result {create_pascal(value)};
 
-    /*int sum {};
-    for(auto elem : row)
-    {
-        sum += elem;
-    }
-    return sum;*/
-    CHECK(result[0][0] == 1);
     CHECK(result.size() == 5);
     CHECK(result[4].size() == 9);
+    CHECK(result[0][0] == 1);
+    CHECK(result[2][2] == 1);
+    CHECK(result[3][1] == 3);
+    CHECK(result[4][2] == 6);
 }
-
-
-/*TEST_CASE("Math util tests")
-{
-    CHECK(sum(1,2) == 3);
-    CHECK(divide(4,0) == -1);
-}*/
