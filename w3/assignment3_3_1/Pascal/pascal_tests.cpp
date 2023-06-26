eb8dfdecd131d57d6d355453cef5eb601e249c11
Kirsi Huhtakallio <kirsi.huhtakallio@gmail.com>
2023-06-26 15:40:39 +0300

    add create_pascal function and unit tests for function

--- w3/assignment3_3_1/Pascal/pascal_tests.cpp
+++ w3/assignment3_3_1/Pascal/pascal_tests.cpp
@@ -2,16 +2,33 @@
 #include "pascal.h"
 #include "doctest.h"
 
+int row = 10;
+std::vector<std::vector<int>> result = create_pascal(row);
 
-TEST_CASE("Pascal vector tests")
+TEST_CASE("Pascal vector")
 {
-    int value = 5;
-    std::vector<std::vector<int>> result = create_pascal(value);
+    SUBCASE("Size of vector")
+    {
+        CHECK(result.size() == row);
+        CHECK(result[0].size() == 1);
+        CHECK(result[4].size() == 5);
+        CHECK(result[row-1].size() == row );
+    }
 
-    CHECK(result.size() == 5);
-    CHECK(result[4].size() == 9);
-    CHECK(result[0][0] == 1);
-    CHECK(result[2][2] == 1);
-    CHECK(result[3][1] == 3);
-    CHECK(result[4][2] == 6);
+    SUBCASE("Single places")
+    {
+        CHECK(result[0][0] == 1);
+        CHECK(result[3][1] == 3);
+        CHECK(result[5][2] == 10);
+    }
+
+    SUBCASE("Check sum of last line")
+    {
+        int sum {};
+        for(auto elem : result[row-1])
+        {
+            sum += elem;
+        }
+        CHECK( pow(2, row-1)== sum);
+    }
 }
