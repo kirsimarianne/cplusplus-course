901df643ca2b6e9687daf4fa7613edda1449e90f
Kirsi Huhtakallio <kirsi.huhtakallio@gmail.com>
2023-07-02 00:35:13 +0300

    Continuing sign in function

--- ass3_1_1/Bank/interface.cpp
+++ ass3_1_1/Bank/interface.cpp
@@ -1,6 +1,7 @@
 #include "interface.h"
 #include <iostream>
 #include <cctype>
+#include <cmath>
 
 int string_to_int(std::string str)
 {
@@ -18,6 +19,37 @@ int string_to_int(std::string str)
         throw e;
     }
     return integer;
+}
+
+double string_to_double(std::string str)
+{
+    double number{};
+    try
+    {
+        number = std::stod(str);
+    }
+    catch (const std::invalid_argument &e)
+    {
+        throw e;
+    }
+    catch (const std::out_of_range &e)
+    {
+        throw e;
+    }
+    return number;
+}
+
+bool max_two_decimals(double number)
+{
+    double integer_part;
+    // modf returns number without integer part, for example 2.34 => 0.34
+    double fractional_part = std::modf(number, &integer_part);
+    // multiply with 100
+    fractional_part *= 100;
+
+    int num_decimals = static_cast<int>(fractional_part);
+
+    return num_decimals <= 99;
 }
 
 /* Function asks information from user: name, address and phone number.
@@ -111,6 +143,37 @@ AccountID get_account_id(UsersMap &users_map, UserID user_id)
     }
 }
 
+double get_amount()
+{
+    while (true)
+    {
+        std::cout << "Please give the amount: ";
+        std::string input;
+        std::getline(std::cin, input);
+        double amount{};
+        try
+        {
+            amount = string_to_int(input);
+        }
+        catch (const std::invalid_argument &e)
+        {
+            std::cerr << "Invalid amount.\n";
+            continue;
+        }
+        catch (const std::out_of_range &e)
+        {
+            std::cerr << "Invalid amount.\n";
+            continue;
+        }
+        if (!(max_two_decimals(amount)) || amount <= 0)
+        {
+            std::cerr << "Invalid amount.\n";
+            continue;
+        }
+        return amount;
+    }
+}
+
 void sign_in(UsersMap &users_map, AccountsMap &accounts_map)
 {
     UserID user_id{get_user_id(users_map)};
@@ -147,25 +210,35 @@ void sign_in(UsersMap &users_map, AccountsMap &accounts_map)
         case 1:
         {
             AccountID account_id{get_account_id(users_map, user_id)};
-            std::cin >> account_id;
-            std::cin.ignore(256, '\n');
-            std::cout << "Give the amount: ";
-            double amount{};
-            std::cin >> amount;
-            if (add_money(user_id, account_id, amount))
+            double amount{get_amount()};
+            if (add_money(users_map, accounts_map, user_id, account_id, amount))
             {
-                std::cout << "Money added succesfully!"
-                          << "\n";
+                std::cout << "Money added succesfully!\n";
             }
+
             else
             {
-                std::cout << "Wrong bank account number."
-                          << "\n";
+                // Should never reach this.
+                // Checking done already in get_account_id function.
+                std::cout << "Function suspended.\n";
             }
             break;
         }
         case 2:
         {
+            AccountID account_id{get_account_id(users_map, user_id)};
+            double amount{get_amount()};
+            if (withdraw_money(users_map, accounts_map, user_id, account_id, amount))
+            {
+                std::cout << "Money withdrawed succesfully!\n";
+            }
+            else
+            {
+                // Should reach only if balance is smaller than amount.
+                // Bank account checking done already in get_account function.
+                std::cout << "Function suspended.\n";
+            }
+            break;
         }
         case 3:
         {
