4074f8e29e03085ab73058827b06a4b2ff24a66a
Kirsi Huhtakallio <kirsi.huhtakallio@gmail.com>
2023-07-01 12:36:33 +0300

    Add comment for the program and const value for maximum 8

---
+++ w1/assignment1_2_2/main.cpp
@@ -0,0 +1,97 @@
+/* The program verify if a password is strong.
+*/
+
+#include <string>
+#include <iostream>
+#include <cctype>
+
+const int MAX = 8;
+
+
+bool length_valid(const std::string &str)
+{
+    if(str.length() < MAX)
+    {
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+
+bool contains_uppercase(const std::string &str)
+{
+    for(int i = 0; i < str.length(); i++)
+    {
+        if(std::isupper(str[i]))
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool contains_lowercase(const std::string &str)
+{
+    for(int i = 0; i < str.length(); i++)
+    {
+        if(std::islower(str[i]))
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+
+bool contains_digit(const std::string &str)
+{
+    for(int i = 0; i < str.length(); i++)
+    {
+        if(std::isdigit(str[i]))
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool contains_punct(const std::string &str)
+{
+    for(int i = 0; i < str.length(); i++)
+    {
+        if(std::ispunct(str[i]))
+        {
+            return true;
+            
+        }
+    }
+    return false;
+}
+
+
+std::string set_password()
+{
+    std::cout << "Enter a password: " << std::endl;
+    std::string password;
+    std:: cin >> password;
+    return password;
+
+}
+
+int main()
+{
+    std::string password {set_password()};
+
+    if(length_valid(password) && contains_uppercase(password) && contains_lowercase(password)
+        && contains_digit(password) && contains_punct(password))
+    {
+        std::cout << "Password is strong." << std::endl;
+    }
+    else{
+        std::cout << "Password is weak." << std::endl;
+    }
+    
+    return 0;
+
+}
