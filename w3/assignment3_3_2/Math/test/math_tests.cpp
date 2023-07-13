d8447898fe20624f3aba6f50eb566ddc3b1d13fa
Kirsi Huhtakallio <kirsi.huhtakallio@gmail.com>
2023-07-13 15:50:43 +0300

    e1 and e2

---
+++ w3/assignment3_3_2/Math/test/math_tests.cpp
@@ -0,0 +1,54 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "../external/doctest.h"
+#include "../includes/math.h"
+
+TEST_CASE("Check sum")
+{
+    CHECK(add(2, 1) == 3);
+    CHECK(add(1, 2) == 3);
+    CHECK(add(-1, 2) == 1);
+    CHECK(add(1, -2) == -1);
+    CHECK(add(-1, -2) == -3);
+    CHECK(add(0, 2) == 2);
+    CHECK(add(0, 0) == 0);
+}
+
+TEST_CASE("Check subtract")
+{
+    CHECK(subtract(2, 1) == 1);
+    CHECK(subtract(1, 2) == -1);
+    CHECK(subtract(-1, 2) == -3);
+    CHECK(subtract(1, -2) == 3);
+    CHECK(subtract(-1, -2) == 1);
+    CHECK(subtract(0, 2) == -2);
+    CHECK(subtract(2, 0) == 2);
+    CHECK(subtract(0, 0) == 0);
+}
+
+TEST_CASE("Check multiply")
+{
+    CHECK(multiply(2, 1) == 2);
+    CHECK(multiply(1, 2) == 2);
+    CHECK(multiply(-1, 2) == -2);
+    CHECK(multiply(1, -2) == -2);
+    CHECK(multiply(-1, -2) == 2);
+    CHECK(multiply(0, 2) == 0);
+    CHECK(multiply(2, 0) == 0);
+    CHECK(multiply(0, 0) == 0);
+}
+
+TEST_CASE("Check divide")
+{
+    CHECK(divide(4.0, 2.0) == 2);
+    CHECK(divide(2.0, 4.0) == 0.5);
+    CHECK(divide(-2.0, 4.0) == -0.5);
+    CHECK(divide(2.0, -4.0) == -0.5);
+    CHECK(divide(-2.0, -4.0) == 0.5);
+    CHECK(divide(0.0, 2.0) == 0);
+    CHECK(divide(1.0, 3.0) == 0.33);
+}
+
+TEST_CASE("Check dividing with 0")
+{
+    CHECK_THROWS_AS(divide(1.0,0.0), std::runtime_error);
+}
