dd965ec130f0479c150accd219db437567adf390
Kirsi Huhtakallio <kirsi.huhtakallio@gmail.com>
2023-08-03 13:08:47 +0300

    Revert "Delete w1 directory"
    This reverts commit 02e6999600c848f5a82807768b09c5417136f508.

---
+++ w1/assignment1_2_3/main.cpp
@@ -0,0 +1,68 @@
+/* The program stores student structs and prints average and highest grade.
+*/
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+
+struct StudentStruct
+{
+    std::string name {};
+    int age {};
+    int grade {};
+};
+
+void set_students(std::vector<StudentStruct> &students_vect)
+{
+    StudentStruct student;
+    std::cout << "Count of students: " << std::endl;
+    int count {};
+    std::cin >> count;
+    for(int i = 0; i < count; i++){
+        std::cout << "Name of student: " << std::endl;
+        std::cin >> student.name;
+        std::cout << "Age of student: " << std::endl;
+        std::cin >> student.age;
+        std::cout << "Grade of student: " << std::endl;
+        std::cin >> student.grade;
+        students_vect.push_back(student);
+    }
+}
+
+void get_average(std::vector<StudentStruct> &students_vect)
+{
+    double sum {0};
+    for(const StudentStruct &student : students_vect)
+    {
+        sum += student.grade;
+    }
+    double average {sum/students_vect.size()};
+    std::cout << "Average is: " << round(average*100.0)/100.0 << std::endl;
+}
+
+void get_highest(std::vector<StudentStruct> &students_vect)
+{
+    int top_grade {0};
+    std::string top_student {};
+    for(const StudentStruct &student : students_vect)
+    {
+        if(student.grade > top_grade)
+        {
+            top_student = student.name;
+        }
+    }
+    std::cout << "The student with the highest grade is: "  << top_student << std::endl;
+}
+
+
+int main()
+{
+    std::vector<StudentStruct> students_vect;
+
+    set_students(students_vect);
+    get_average(students_vect);
+    get_highest(students_vect);
+
+    return 0;
+}
